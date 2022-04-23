42a7537d2328e073046aefb6fce16481027c17bb
jefersonar-cmd <jefersonaraujo86@protonmail.com>
2022-04-22 21:09:11 -0300

    Atualizando Repositório

---
+++ Linguaguem de Programação Python/ExibirVaviavel.py
@@ -0,0 +1,9 @@
+codigo = 105
+nome = 'José Santana'
+salario = 1650.00
+ativo = True
+
+print(f'Código: {codigo}')
+print(f'Nome: {nome}')
+print(f'Salário: R${salario}')
+print(f'Ativo: {ativo}')
---
+++ Linguaguem de Programação Python/Operadores.py
@@ -0,0 +1,4 @@
+A, B, C = 5, 15, 20
+print("A == B AND B > C: ", A == B and B > C)
+print("A < B OR B > C: ", A < B and B > C)
+print("NOT B > C: ", not A == B)
